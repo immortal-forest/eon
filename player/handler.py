8d99be286890d6dc17f9cecb55fda2ec1e625a61
immortal-forest <ali.salman.sa.01@gmail.com>
2025-08-20 21:34:47 +0800

    feat(player): save and load player state

---
+++ player/handler.py
@@ -0,0 +1,32 @@
+import json
+from dataclasses import asdict
+
+from data import Player, GameFileError
+
+
+class PlayerJsonEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Player):
+            return asdict(obj)
+        return super().default(self, obj)
+
+
+def save_game(player: Player):
+    try:
+        with open("player/state.json", "w") as file:
+            json.dump(player, file, cls=PlayerJsonEncoder, indent=4)
+    except (FileNotFoundError, IOError):
+        raise GameFileError("Could not save game state")
+
+
+def load_game():
+    try:
+        with open("player/state.json", "r") as file:
+            data = json.load(file)
+            return Player(**data)
+    except FileNotFoundError:
+        GameFileError("No save file found.")
+        return None
+    except (json.JSONDecodeError, TypeError):
+        GameFileError(f"Could not load save file. It may be corrupted")
+        return None
