9ed9455fac13399a16a007b9902ac7a1ea38542f
immortal-forest <ali.salman.sa.01@gmail.com>
2025-08-23 15:55:43 +0800

    fix(player): save and load functions

--- player/handler.py
+++ player/handler.py
@@ -2,31 +2,49 @@ import json
 from dataclasses import asdict
 
 from data import GameFileError, Player
+from data.models import Enemy
 
 
-class PlayerJsonEncoder(json.JSONEncoder):
+class EntityJsonEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, Player):
             return asdict(obj)
         return super().default(self, obj)
 
 
-def save_game(player: Player):
+def save_game(
+    player: Player, enemy: Enemy | None, scene_name: str, combat_log: list | None
+):
     try:
-        with open("player/state.json", "w") as file:
-            json.dump(player, file, cls=PlayerJsonEncoder, indent=4)
+        with open("data/state.json", "w") as file:
+            player_json = json.dumps(player, cls=EntityJsonEncoder)
+            if enemy is not None:
+                enemy_json = json.dumps(enemy, cls=EntityJsonEncoder)
+            json.dump(
+                {
+                    "player": player_json,
+                    "enemy": None if enemy is None else enemy_json,
+                    "scene_name": scene_name,
+                    "combat_log": [] if not combat_log else combat_log,
+                },
+                file,
+            )
     except (FileNotFoundError, IOError):
         raise GameFileError("Could not save game state")
 
 
 def load_game():
     try:
-        with open("player/state.json", "r") as file:
+        with open("data/state.json", "r") as file:
             data = json.load(file)
-            return Player(**data)
+            player = Player(**json.loads(data["player"]))
+            enemy = data["enemy"] if data["enemy"] is None else Enemy(**data["enemy"])
+            scene_name = data["scene_name"]
+            combat_log = data["combat_log"]
+            return player, enemy, scene_name, combat_log
     except FileNotFoundError:
-        GameFileError("No save file found.")
-        return None
-    except (json.JSONDecodeError, TypeError):
-        GameFileError("Could not load save file. It may be corrupted")
-        return None
+        raise GameFileError("No save file found.")
+
+    except (json.JSONDecodeError, TypeError) as e:
+        print(e)
+        raise GameFileError("Could not load save file. It may be corrupted")
